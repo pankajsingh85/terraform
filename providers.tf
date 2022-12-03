bd6050d5598f59d2e877b21cc61f18a37be3f043
Pankaj Singh <52364179+pankajsingh85@users.noreply.github.com>
2022-12-03 11:41:48 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,12 @@
 provider "azurerm" {
 
   # Configuration options
-  subscription_id = "03e358a9-ffed-4ad1-8eca-44a40446a295"
-  client_id       = "7a0746b4-34c3-47c2-bb39-d68f6f5882fa"
-  client_secret   = "b298Q~Mx3sgaML64PPwY2cEdiWSrPw0BXCNgqcdq"
-  tenant_id       = "e14e73eb-5251-4388-8d67-8f9f2e2d5a46"
 
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  # tenant_id       = var.tenant_id
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 
   features {
 
